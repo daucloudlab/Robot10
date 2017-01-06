ddd9c289be77893c6fc8963566e07dbb137065b3
daulet <daucloudlab@gmail.com>
2017-01-06 13:55:29 +0600

    version 01

---
+++ src/main/java/kz/kaznitu/lessons/main/BaseModel.java
@@ -0,0 +1,57 @@
+package kz.kaznitu.lessons.main;
+
+import kz.kaznitu.lessons.interfaces.Hand;
+import kz.kaznitu.lessons.interfaces.Head;
+import kz.kaznitu.lessons.interfaces.Leg;
+import kz.kaznitu.lessons.interfaces.Robot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public abstract class BaseModel implements Robot {
+
+    @Autowired
+    @Qualifier("sonyHand")
+    private Hand hand;
+
+    @Autowired(required = false)
+    private Leg leg;
+
+    @Autowired
+    private Head head;
+
+    public BaseModel() {
+        System.out.println(this + " - BaseModel constructor()");
+    }
+
+    public BaseModel(Hand hand, Leg leg, Head head) {
+        this();
+        this.hand = hand;
+        this.leg = leg;
+        this.head = head;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Leg getLeg() {
+        return leg;
+    }
+
+    public void setLeg(Leg leg) {
+        this.leg = leg;
+    }
+
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
+    }
+
+}
